dd86f58f0141bc4721e6e82098bd7f09337f4179
bairongyu <957820957@qq.com>
2022-03-12 22:40:20 +0800

    feat: course1

--- src/pages/course01/index.tsx
+++ src/pages/course01/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Card } from 'antd';
+import { InitDemo } from './gl';
 
 const index: React.FC = () => {
+  const ref = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvasElement = ref.current;
+
+    if (!canvasElement) return;
+
+    InitDemo(canvasElement);
+  }, [ref]);
+
   return (
     <Card>
-      <canvas>你的浏览器不支持canvas</canvas>
+      <canvas ref={ref} width="800" height="600">
+        你的浏览器不支持canvas
+      </canvas>
     </Card>
   );
 };
